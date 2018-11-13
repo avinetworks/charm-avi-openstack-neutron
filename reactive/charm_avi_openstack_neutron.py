44f464a924030f888d5229ad30dfb8d30a520218
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2018-11-13 14:33:36 +0530

    Added config change trigger

--- reactive/charm_avi_openstack_neutron.py
+++ reactive/charm_avi_openstack_neutron.py
@@ -22,7 +22,7 @@ def install_charm_avi_openstack_neutron():
     set_flag('charm-avi-openstack-neutron.installed')
 
 
-@when('neutron-plugin-api-subordinate.connected')
+@when('neutron-plugin-api-subordinate.connected', 'config.changed')
 def configure_principle(api_principle):
 
     avi_config = {
